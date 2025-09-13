975fe069dd6031c84203ac2aa924774dcf6d1de0
arun4finfree <arun4finfree@gmail.com>
2025-09-13 21:54:55 +0530

    Updated url in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReceiptlyPlus/',
+  //base: '/ReceiptlyPlus/',
+  base: process.env.NODE_ENV === 'production' ? '/ReceiptlyPlus/' : '/',
 })
